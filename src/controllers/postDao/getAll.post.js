9f2fa2ddf71a2486cd7878d1bfc99695f4052e14
Gabriel Rodriguez <garodriguezm96@gmail.com>
2020-12-16 02:40:43 -0300

    Create new post data whitout content value and separate sort function

--- src/controllers/postDao/getAll.post.js
+++ src/controllers/postDao/getAll.post.js
@@ -4,15 +4,24 @@ const Post = db.posts;
 
 export default (req, res) => {
 
-    /** Get all posts for the database. */
 
+    /**Create a new post data without content value */
+    const postStructure = (post) => {
+        const { id, title, image, categoryId, createdAt } = post;
+        const newPost = { id, title, image, categoryId, createdAt };
+        return newPost;
+    }
+
+    /** Turn strings into dates, and then subtract them to get a value that is either negative, positive, or zero. */
+    const sortData = (a, b) => {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+    }
+
+    /** Get all posts for the database. */
     Post.findAll()
         .then(data => {
-            const sortByDate = data.sort(function (a, b) {
-                /** Turn your strings into dates, and then subtract them
-                /* to get a value that is either negative, positive, or zero. */
-                return new Date(b.createdAt) - new Date(a.createdAt);
-            });;
+            const newPosts = data.map(postStructure);
+            const sortByDate = newPosts.sort(sortData);
             res.send(sortByDate);
         })
         .catch(err => {
