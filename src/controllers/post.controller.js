acbc8a4361d55d0ce419b32b5536e6d74c2ccc9b
Gabriel Rodriguez <garodriguezm96@gmail.com>
2020-12-17 12:07:55 -0300

    Edit folder

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,7 +1,7 @@
-import createPost from "./postDao/create.post.js";
-import getAllPost from "./postDao/getAll.post.js";
-import getIdPost from "./postDao/getId.post.js";
-import updatePost from "./postDao/update.post.js";
-import deletePost from "./postDao/delete.post.js";
+import createPost from "./post.crud/create.post.js.js";
+import getAllPost from "./post.crud/getAll.post.js.js";
+import getIdPost from "./post.crud/getId.post.js.js";
+import updatePost from "./post.crud/update.post.js";
+import deletePost from "./post.crud/delete.post.js.js";
 
 export { createPost, getAllPost, getIdPost, updatePost, deletePost };
